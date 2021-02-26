91c8576ba9953450b7ef497b3a4b21d53014bbed
Rafael Zorzenon <rafaasimi@hotmail.com>
2021-02-26 10:23:36 -0300

    Added level count dynamic

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
 import styles from "../styles/components/Profile.module.css";
 
 const Profile = () => {
+  const { level } = useContext(ChallengesContext)
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/rafaasimi.png" alt="Rafael Simionato" />
@@ -9,7 +12,7 @@ const Profile = () => {
         <strong>Rafael Simionato</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
